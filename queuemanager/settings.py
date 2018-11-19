24fa9c52613aad5978b43dae08a7a886e9a4dd2c
rupin <rupin.chheda@gmail.com>
2018-11-19 11:52:16 +0530

    New Files

--- queuemanager/settings.py
+++ queuemanager/settings.py
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'queuemanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
