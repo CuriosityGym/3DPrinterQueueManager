45db6ff61bf2da8f8f6e201d80e8d7f6700e019a
rupin <rupin.chheda@gmail.com>
2018-12-08 17:05:52 +0530

    Added Download Link on Schedule Page

--- queuemanager/static/JS/queueManager.js
+++ queuemanager/static/JS/queueManager.js
@@ -2,5 +2,13 @@ $( document ).ready(function() {
     
 	$(".status_change_dd").change(function(){
 		console.log($(this).attr('job_id'));
+		$(this).siblings(":button").prop('disabled', false);
 	});
+	
+	$(".status_change_btn").click(function(){
+		
+		console.log($(this).attr('job_id'));
+	});
+	
+	
 });
