248bf587c883d2e33ae0aaee64a04e2f56be4765
rupin <rupin.chheda@gmail.com>
2018-12-11 14:01:15 +0530

    Added Download Link on Schedule Page

--- queuemanager/ajaxendpoints.py
+++ queuemanager/ajaxendpoints.py
@@ -16,6 +16,7 @@ from datetime import *
 from django.urls import reverse
 
 from datetime import datetime
+from django.core import serializers
 
 
 @login_required(login_url='/login/')
@@ -27,9 +28,41 @@ def Printing(request, jobid):
     
 @login_required(login_url='/login/')
 def getPrintingList(request):
-    if(request.user.is_superuser):
-        #Job.objects.filter(job_id=jobid).update(status='Printing')
-        return HttpResponse("Ok, sending printing list")
+    
+
+      
+    printingList=[]
+    
+    if request.user.is_superuser:
+        
+        printing = list(Job.objects.filter(status = "Printing").select_related("fk_profile"))       
+        for i in range(0, len(printing)):
+            printingList.append(printing[i])
+
+        
+
+    else:
+        
+        printing = list(Job.objects.filter(status = "Printing").filter(fk_profile = util.getProfile(request.user)))
+ 
+        for i in range(0, len(printing)):
+            printingList.append(printing[i])
+
+    
+
+    for i in range(0, len(printingList)):
+        printingList[i].endDate = printingList[i].print_end_time.strftime("%d %b %Y")
+    
+
+    util = Util()
+    context = util.getQuota(request.user)   
+    context['PrintingContextObject'] = printingList
+    return HttpResponse(
+        serializers.serialize("json", context),
+        content_type="application/json"
+    )
+
+   
     
     
 
