728e287c0dfc2e002ca771e7490e87fddda407e1
rupin <rupin.chheda@gmail.com>
2018-12-15 13:12:07 +0530

    Addmin Import Export

--- queuemanager/admin.py
+++ queuemanager/admin.py
@@ -8,14 +8,28 @@ from django.contrib.auth.models import User
 admin.site.register(Job)
 admin.site.register(FeaturedPrint)
 admin.site.register(RecentPrint)
-#admin.site.register(Profile)
 
 
 
+#Add Upload Options for Users
+class UserResource(resources.ModelResource):
+    class Meta:
+        model = User
+        fields=('id', 'password','username','first_name','last_name','email')
+        
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+
+# Add Upload Options for Profiles
 class ProfileResource(resources.ModelResource):
     class Meta:
         model = Profile
-        fields=('user__id', 'user__password','user__username','user__first_name','user__last_name','user__email', 'quota', 'grade')
+        #fields=('id', 'password','username','first_name','last_name','email')
         
 class ProfileAdmin(ImportExportModelAdmin):
     resource_class = ProfileResource
