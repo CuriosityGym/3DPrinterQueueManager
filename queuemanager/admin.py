5ec13a5a879cdd99c42aa42216913f1be7a916f0
rupin <rupin.chheda@gmail.com>
2018-12-15 11:20:34 +0530

    Added Printed Action Buttons Completed/Failed

--- queuemanager/admin.py
+++ queuemanager/admin.py
@@ -16,7 +16,7 @@ class UserResource(resources.ModelResource):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'profile__quota')
-        exclude = ('id')
+        exclude = ('id',)
 
 class UserAdmin(BaseAdmin, ImportExportModelAdmin):
     resource_class = UserResource
