58e5f5ec2be2177d9cf2764fb235bb0d435bde06
rupin <rupin.chheda@gmail.com>
2018-12-15 12:24:53 +0530

    Added Printed Action Buttons Completed/Failed

--- queuemanager/admin.py
+++ queuemanager/admin.py
@@ -15,7 +15,7 @@ admin.site.register(Profile)
 class UserResource(resources.ModelResource):
     class Meta:
         model = User
-        #fields=('id', 'password','username','first_name','last_name','email', 'is_superuser', 'is_staff')
+        fields=('id', 'password','username','first_name','last_name','email', 'is_superuser', 'is_staff','is_active')
         
 class UserAdmin(ImportExportModelAdmin):
     resource_class = UserResource
