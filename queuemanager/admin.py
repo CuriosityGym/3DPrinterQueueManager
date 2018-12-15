cee7435552bf89daa8e3602e6732ad08eb33639f
rupin <rupin.chheda@gmail.com>
2018-12-15 12:49:44 +0530

    Addmin Import Export

--- queuemanager/admin.py
+++ queuemanager/admin.py
@@ -15,7 +15,7 @@ admin.site.register(Profile)
 class UserResource(resources.ModelResource):
     class Meta:
         model = User
-        fields=('id', 'password','username','first_name','last_name','email')
+        fields=('id', 'password','username','first_name','last_name','email', 'profile__quota', 'profile__grade')
         
 class UserAdmin(ImportExportModelAdmin):
     resource_class = UserResource
